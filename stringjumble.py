7d0031098ad24f9a7a419342d897bc0763a6f874
jaEimele24 <46973541+jaEimele24@users.noreply.github.com>
2019-02-26 14:54:53 -0500

    Update stringjumble.py

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: James
+Credit: Mr Dennison
 
 Assignment:
 
@@ -25,3 +25,18 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+text=input("Please enter a string of text (the bigger the better): ")
+print("You entered ", text ,". Now Jumble it:")
+#first part
+row=text
+print(row[::-1])
+#second part
+list=(text.split())
+list.reverse()
+print(" ".join(list))
+#third part
+collum=[text]
+collum1=(text.split())
+collum2=[ x[::-1] for x in collum1]
+collum3=(" ".join(collum2))
+print(collum3)
