f7845e2f3fa83339def5dc053200f2a0bbfd04c2
jaEimele24 <46973541+jaEimele24@users.noreply.github.com>
2019-02-27 13:40:45 -0500

    Update stringjumble.py

--- stringjumble.py
+++ stringjumble.py
@@ -26,7 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 text=input("Please enter a string of text (the bigger the better): ")
-s="You entered "'"{0}"'". Now Jumble it:"
+s="You entered "'"{0}"'". Now jumble it:"
 print(s.format(text))
 print()
 #first part
